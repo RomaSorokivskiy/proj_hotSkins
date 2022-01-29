7b478560480e2c7811257fd6a40dd1b1be0656b3
sorok <sorokivroma@gmail.com>
2022-01-30 00:53:05 +0300

    update languge selector

--- src/script/script.js
+++ src/script/script.js
@@ -9,23 +9,12 @@ window.addEventListener('DOMContentLoaded', function(){
     const mainWord = document.getElementById('main_word');
     const connectProj = document.getElementById('connect_proj');
 
-    languageSelector.addEventListener('click', ()=>{
-        if(!languageItem[1].classList.contains('select')){
-            languageItem[1].classList.add('select');
-            languageItem[0].classList.remove('select');
-            translationEng();
-        }else{
-            languageItem[1].classList.remove('select');
-            languageItem[0].classList.add('select');
-            translationRus();
-        }
+    languageItem[1].addEventListener('click',()=>{
 
         function translateText(elemet, text){
             elemet.innerText = `${text}`;
             console.log('work');
         }
-
-        function translationEng(){
             translateText(contactButtons[0], "Help");
             translateText(contactButtons[1], "Contact");
             translateText(logIn,"LogIn");
@@ -36,18 +25,38 @@ window.addEventListener('DOMContentLoaded', function(){
             translateText(infoHelp[0],"Help");
             translateText(infoHelp[1],"Contact");
             translateText(infoHelp[2],"Terms of use");
+
+            languageItem[0].classList.remove('select');
+    });
+
+    languageItem[0].addEventListener('click',()=>{
+        
+        function translateText(elemet, text){
+            elemet.innerText = `${text}`;
+            console.log('work');
         }
-        function translationRus(){
+
             translateText(contactButtons[0], "Помощь");
             translateText(contactButtons[1], "Контакты");
             translateText(logIn,"Войти");
-
+    
             translateText(mainWord,"Принимайте платежи от пользователейс помощью скинов CS:GO и DOTA 2");
             translateText(connectProj,"Подключить проект");
-
+    
             translateText(infoHelp[0],"Помощь");
             translateText(infoHelp[1],"Контакты");
             translateText(infoHelp[2],"Пользовательское соглашение");
+
+            languageItem[1].classList.remove('select');
+    });
+
+    languageSelector.addEventListener('click', ()=>{
+        if(!languageSelector.classList.contains('active_selector')){
+            languageSelector.classList.add('active_selector');
+            languageItem[0].classList.add('select');
+            languageItem[1].classList.add('select');
+        }else{
+            languageSelector.classList.remove('active_selector');
         }
     });
 });
